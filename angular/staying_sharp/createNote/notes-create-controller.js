d4609cbada982b9f68ef58e8105bf99be994dac4
granam180 <bonneville.joey@gmail.com>
2015-09-25 16:47:38 -0400

    removing dir

--- angular/staying_sharp/createNote/notes-create-controller.js
+++
@@ -1,6 +0,0 @@
-angular.module('NoteWrangler')
-.controller('NotesCreateController', [function() {
-  this.saveNote = function(note){
-    $http({method: 'POST', url: '/notes', data: note});
-  };
-}]);
--- angular/staying_sharp/singleNote/notes-show-controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('NoteWrangler')
-.controller('NotesShowController', function($http, $routeParams) {
-  var controller = this;
-  $http({ method: 'GET', url: '/notes/' + $routeParams.id })
-  .success(function(data) {
-  	controller.note = data;
-  });
-});
